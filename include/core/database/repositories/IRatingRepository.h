6327ac9b75a60d4780873e361c3cd453dd52c3b1
Belyashik2K <lovelybelyashik@gmail.com>
2025-04-07 21:04:12 +0300

    Add PostgreSQLRatingRepository for managing rating history and leaderboard

--- include/core/database/repositories/IRatingRepository.h
+++ include/core/database/repositories/IRatingRepository.h
@@ -7,13 +7,19 @@
 
 #include <any>
 #include <optional>
+#include <string>
 
 class IRatingRepository {
 public:
     virtual ~IRatingRepository() = default;
-
     virtual std::optional<std::any> getLeaderboard(int limit) const = 0;
-    virtual std::optional<std::any> getRatingHistory(int userId, int limit) const = 0;
+    virtual std::optional<std::any> getRatingHistoryByUserId(int userId, int limit) const = 0;
+    virtual int createRatingHistoryRecord(
+        int userId,
+        std::optional<int> gameId,
+        int rating,
+        const std::string &comment
+    ) const = 0;
 };
 
 #endif //IRATINGREPOSITORY_H
---
+++ include/models/database/PostgreSQL/repositories/PostgreSQLRatingRepository.h
@@ -0,0 +1,25 @@
+//
+// Created by Belyashik2K on 07.04.2025.
+//
+
+#ifndef POSTGRESQLRATINGREPOSITORY_H
+#define POSTGRESQLRATINGREPOSITORY_H
+
+#include "core/database/repositories/IRatingRepository.h"
+#include "models/database/PostgreSQL/PostgreSQLDatabase.h"
+
+class PostgreSQLRatingRepository final : public IRatingRepository {
+    std::shared_ptr<PostgreSQLDatabase> database;
+public:
+    PostgreSQLRatingRepository(std::shared_ptr<PostgreSQLDatabase> database);
+    std::optional<std::any> getLeaderboard(int limit) const override;
+    std::optional<std::any> getRatingHistoryByUserId(int userId, int limit) const override;
+    int createRatingHistoryRecord(
+        int userId,
+        std::optional<int> gameId,
+        int rating,
+        const std::string &comment
+    ) const override;
+};
+
+#endif //POSTGRESQLRATINGREPOSITORY_H
