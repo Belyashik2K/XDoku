b3924ece1e4c3af76592edd40c81c98f22d75420
belyashik2k <lovelybelyashik@gmail.com>
2025-04-29 15:14:48 +0300

    Refactor ImguiLoginView to enhance layout structure and improve button creation

--- src/views/imgui/login/ImguiLoginView.cpp
+++ src/views/imgui/login/ImguiLoginView.cpp
@@ -4,73 +4,75 @@
 
 #include "views/imgui/login/ImguiLoginView.h"
 
-#include <iostream>
-#include <glad/glad.h>
 #include <imgui.h>
 
-#include "managers/TextureManager.h"
 #include "views/imgui/ImguiChildWindow.h"
 #include "views/imgui/ImguiWindow.h"
 
 void ImguiLoginView::render() {
     ImguiWindow window("Login Menu", ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
-    updateBackground("../assets/textures/background.jpg");
+    {
+        updateBackground("../assets/textures/background.jpg");
 
-    constexpr int childWidth = 650;
-    constexpr int childHeight = 350;
-    constexpr ImVec2 childSize(childWidth, childHeight);
+        {
+            constexpr int childWidth = 650;
+            constexpr int childHeight = 350;
+            constexpr ImVec2 childSize(childWidth, childHeight);
 
-    const ImVec2 windowSize = ImGui::GetWindowSize();
-    ImVec2 childPosition;
-    childPosition.x = (windowSize.x - childSize.x) * 0.5f;
-    childPosition.y = (windowSize.y - childSize.y) * 0.5f;
+            const ImVec2 windowSize = ImGui::GetWindowSize();
+            ImVec2 childPosition;
+            childPosition.x = (windowSize.x - childSize.x) * 0.5f;
+            childPosition.y = (windowSize.y - childSize.y) * 0.5f;
 
-    ImGui::SetCursorPos(childPosition);
+            ImGui::SetCursorPos(childPosition);
 
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
 
-    ImguiChildWindow childWindow(
-        "CenteredChild",
-        childSize,
-        ImGuiChildFlags_AutoResizeX | ImGuiChildFlags_AutoResizeY | ImGuiChildFlags_AlwaysAutoResize
-    );
+            ImguiChildWindow childWindow(
+                "CenteredChild",
+                childSize,
+                ImGuiChildFlags_AutoResizeX | ImGuiChildFlags_AutoResizeY | ImGuiChildFlags_AlwaysAutoResize
+            );
 
-    ImGui::PopStyleVar();
+            ImGui::PopStyleVar();
 
-    float textWidth = ImGui::CalcTextSize("Sign in to XDoku").x;
-    ImGui::SetCursorPosX((childWidth - textWidth) * 0.5f);
-    ImGui::Text("Sign in to XDoku");
-    addVerticalSpacing(6);
+            float textWidth = ImGui::CalcTextSize("Sign in to XDoku").x;
+            ImGui::SetCursorPosX((childWidth - textWidth) * 0.5f);
+            ImGui::Text("Sign in to XDoku");
+            addVerticalSpacing(6);
 
-    ImGui::PushItemWidth(childWidth);
-    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(15.0f, 15.0f));
-    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
-    ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
-    ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 1.0f);
+            ImGui::PushItemWidth(childWidth);
+            ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(15.0f, 15.0f));
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+            ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+            ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 1.0f);
 
-    ImGui::InputText("##Username", presenter->getUsername(), presenter->getBufferSize(), ImGuiInputTextFlags_CharsNoBlank);
-    addVerticalSpacing();
-    ImGui::InputText("##Password", presenter->getPassword(), presenter->getBufferSize(), ImGuiInputTextFlags_Password);
-    addVerticalSpacing(6);
-    ImGui::PopItemWidth();
+            ImGui::InputText("##Username", presenter->getUsername(), presenter->getBufferSize(), ImGuiInputTextFlags_CharsNoBlank);
+            addVerticalSpacing();
+            ImGui::InputText("##Password", presenter->getPassword(), presenter->getBufferSize(), ImGuiInputTextFlags_Password);
+            addVerticalSpacing(6);
+            ImGui::PopItemWidth();
 
 
-    const auto &buttonSize = ImVec2(childWidth, 55);
-    ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 10.0f);
+            const auto &buttonSize = ImVec2(childWidth, 55);
+            ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 10.0f);
 
-    createButton("login_button", "Sign in", buttonSize,
-        [this] {
-            presenter->onLoginButtonClicked();
+            createButton("login_button", "Sign in", buttonSize,
+                [this] {
+                    presenter->onLoginButtonClicked();
+                }
+            );
+            addVerticalSpacing();
+
+            createButton("signup_button", "Sign up", buttonSize,
+                [this] {
+                    presenter->onSignUpButtonClicked();
+                }
+            );
+
+            ImGui::PopStyleColor(2);
+            ImGui::PopStyleVar(3);
         }
-    );
-    addVerticalSpacing();
+    }
 
-    createButton("signup_button", "Sign up", buttonSize,
-        [this] {
-            presenter->onSignUpButtonClicked();
-        }
-    );
-
-    ImGui::PopStyleColor(2);
-    ImGui::PopStyleVar(3);
 }
