fb77aef73935f877b83f58fc3f2a3f087a849c9f
belyashik2k <lovelybelyashik@gmail.com>
2025-05-03 15:40:38 +0300

    Refactor frame handler initialization and improve error logging in run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+
 #include "core/Application.h"
 #include "core/AppMediator.h"
 #include "core/EventBus.h"
+
 #include "managers/GameManager.h"
 #include "managers/SessionManager.h"
 
@@ -10,13 +12,13 @@
 #include "models/database/PostgreSQL/repositories/PostgreSQLRatingRepository.h"
 #include "models/database/PostgreSQL/repositories/PostgreSQLGameRepository.h"
 #include "models/database/PostgreSQL/repositories/PostgreSQLMoveRepository.h"
-#include "presenters/MainMenuPresenter.h"
 
+#include "presenters/MainMenuPresenter.h"
 #include "presenters/SignInPresenter.h"
+
 #include "views/imgui/ImguiFrameHandler.h"
 #include "views/imgui/leaderboard/ImguiLeaderboardView.h"
 #include "views/imgui/main_menu/ImguiMainMenuView.h"
-
 #include "views/imgui/sign_in/ImguiSignInView.h"
 #include "views/imgui/sign_up/ImguiSignUpView.h"
 
@@ -38,6 +40,7 @@ int main() {
         const auto sessionManager = std::make_shared<SessionManager>(eventBus.get(), sessionRepository.get());
         const auto gameManager = std::make_shared<GameManager>(eventBus.get(), gameRepository.get());
 
+        auto frameHandler = std::make_unique<ImguiFrameHandler>("XDoku");
         const auto signInView = std::make_shared<ImguiSignInView>();
         const auto signInPresenter = std::make_shared<SignInPresenter>(eventBus.get(), userRepository.get());
         const auto signUpView = std::make_shared<ImguiSignUpView>();
@@ -65,12 +68,11 @@ int main() {
         appMediator->setMainMenuPresenter(mainMenuPresenter.get());
         appMediator->setLeaderboardPresenter(leaderboardPresenter.get());
 
-        auto frameHandler = std::make_unique<ImguiFrameHandler>("XDoku");
         const Application app(std::move(frameHandler), appMediator.get(), eventBus.get());
         app.start();
 
     } catch (const std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+        printf("Oops! An error occurred: %s\n", e.what());
     }
 
     return 0;
