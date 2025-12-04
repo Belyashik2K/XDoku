2bad2606034e156a99858b9a6182dc0458b7203d
belyashik2k <lovelybelyashik@gmail.com>
2025-12-04 08:17:36 +0300

    Add ProfilingManager for function execution tracking and integrate with SudokuGameFactory

---
+++ include/application/managers/ProfilingManager.h
@@ -0,0 +1,70 @@
+//
+// Created by belyashik2k on 12/4/25.
+//
+
+#ifndef XDOKU_PROFILINGMANAGER_H
+#define XDOKU_PROFILINGMANAGER_H
+
+#include <iostream>
+#include <chrono>
+#include <map>
+#include <string>
+#include <functional>
+
+struct FunctionStats {
+    long long totalTime = 0;
+    int callCount = 0;
+};
+
+class FunctionTimerManager {
+public:
+    static FunctionTimerManager& instance() {
+        static FunctionTimerManager instance;
+        return instance;
+    }
+
+    template <typename Func, typename... Args>
+    auto track(const std::string& functionName, Func&& func, Args&&... args) {
+        auto start = std::chrono::high_resolution_clock::now();  // Засекаем время
+
+        auto result = std::forward<Func>(func)(std::forward<Args>(args)...);
+
+        auto end = std::chrono::high_resolution_clock::now();  // Засекаем время завершения
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+        updateStats(functionName, duration.count());
+
+        return result;
+    }
+
+    void updateStats(const std::string& functionName, long long timeTaken) {
+        stats[functionName].totalTime += timeTaken;
+        stats[functionName].callCount++;
+    }
+
+    ~FunctionTimerManager() {
+        printf("============= Function Execution Statistics =============\n");
+        for (const auto& entry : stats) {
+            const auto& functionName = entry.first;
+            const auto& stat = entry.second;
+            double averageTime = static_cast<double>(stat.totalTime) / stat.callCount;
+            std::cout << "| " << std::setw(20) << std::left << functionName
+            << " | total: " << std::setw(15) << std::right << stat.totalTime / 1'000'000.0
+            << " | calls: " << std::setw(10) << std::right << stat.callCount
+            << " | avg: " << std::setw(17) << std::right << averageTime / 1'000.0 << " |\n";
+            printf("=========================================================\n");
+        }
+    }
+
+private:
+    FunctionTimerManager() = default;
+
+    FunctionTimerManager(const FunctionTimerManager&) = delete;
+    FunctionTimerManager& operator=(const FunctionTimerManager&) = delete;
+    FunctionTimerManager(FunctionTimerManager&&) = delete;
+    FunctionTimerManager& operator=(FunctionTimerManager&&) = delete;
+
+    std::map<std::string, FunctionStats> stats;
+};
+
+#endif //XDOKU_PROFILINGMANAGER_H
--- src/domain/sudoku/utils/SudokuFactory.cpp
+++ src/domain/sudoku/utils/SudokuFactory.cpp
@@ -4,11 +4,18 @@
 
 #include "domain/sudoku/utils/SudokuFactory.h"
 
+#include "application/managers/ProfilingManager.h"
 #include "domain/sudoku/utils/SudokuGenerator.h"
 
 std::unique_ptr<SudokuGame> SudokuGameFactory::createNewGame(int userId, SudokuDifficultyEnum difficulty) {
+    FunctionTimerManager& manager = FunctionTimerManager::instance();
     SudokuGenerator generator;
-    const auto [grid, solutionGrid] = generator.generate(difficulty);
+
+    const auto [grid, solutionGrid] = manager.track(
+        "SudokuGenerator::generate",
+        [&generator](const SudokuDifficultyEnum difficulty) { return generator.generate(difficulty); },
+        difficulty
+    );
 
     return std::make_unique<SudokuGame>(
         userId,
